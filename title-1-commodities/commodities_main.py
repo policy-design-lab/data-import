f5965c4be7d8dd503a14ab1cd07e73c302352a1e
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2023-05-11 13:36:34 -0500

    Change JSON strucuture for state distribution.

--- title-1-commodities/commodities_main.py
+++ title-1-commodities/commodities_main.py
@@ -24,6 +24,58 @@ class CommoditiesDataParser:
         self.processed_data_dict = dict()
         self.state_distribution_data_dict = dict()
         self.programs_data_dict = dict()
+        self.us_state_abbreviation = {
+            'AL': 'Alabama',
+            'AK': 'Alaska',
+            'AZ': 'Arizona',
+            'AR': 'Arkansas',
+            'CA': 'California',
+            'CO': 'Colorado',
+            'CT': 'Connecticut',
+            'DE': 'Delaware',
+            'FL': 'Florida',
+            'GA': 'Georgia',
+            'HI': 'Hawaii',
+            'ID': 'Idaho',
+            'IL': 'Illinois',
+            'IN': 'Indiana',
+            'IA': 'Iowa',
+            'KS': 'Kansas',
+            'KY': 'Kentucky',
+            'LA': 'Louisiana',
+            'ME': 'Maine',
+            'MD': 'Maryland',
+            'MA': 'Massachusetts',
+            'MI': 'Michigan',
+            'MN': 'Minnesota',
+            'MS': 'Mississippi',
+            'MO': 'Missouri',
+            'MT': 'Montana',
+            'NE': 'Nebraska',
+            'NV': 'Nevada',
+            'NH': 'New Hampshire',
+            'NJ': 'New Jersey',
+            'NM': 'New Mexico',
+            'NY': 'New York',
+            'NC': 'North Carolina',
+            'ND': 'North Dakota',
+            'OH': 'Ohio',
+            'OK': 'Oklahoma',
+            'OR': 'Oregon',
+            'PA': 'Pennsylvania',
+            'RI': 'Rhode Island',
+            'SC': 'South Carolina',
+            'SD': 'South Dakota',
+            'TN': 'Tennessee',
+            'TX': 'Texas',
+            'UT': 'Utah',
+            'VT': 'Vermont',
+            'VA': 'Virginia',
+            'WA': 'Washington',
+            'WV': 'West Virginia',
+            'WI': 'Wisconsin',
+            'WY': 'Wyoming'
+        }
 
     def find_program_by_subprogram(self, program_description):
         for program_name in self.programs_subprograms_mapping:
@@ -77,6 +129,13 @@ class CommoditiesDataParser:
         # Filter only relevant years' data
         commodities_data = commodities_data[commodities_data["pay_year"].between(self.start_year, self.end_year,
                                                                                  inclusive="both")]
+
+        # Exclude programs that are not included at present
+        commodities_data = commodities_data[
+            (commodities_data["program_description"] != "Ad hoc or Supplemental") &
+            (commodities_data["program_description"] != "Market Facilitation Program (MFP)") &
+            (commodities_data["program_description"] != "Coronavirus Food Assistance Program (CFAP)")
+            ]
 
         # Group data by state, program description, and payment
         payments_by_program_by_state_for_year = \
@@ -270,12 +329,14 @@ class CommoditiesDataParser:
             total_payments_by_program_at_national_level = round(
                 commodities_data[["program_description", "payments"]].groupby(["program_description"]).sum(), 2)
 
-            # Iterate through all tuples
-            for state_name, payment in total_payments_by_state.items():
-                yearly_state_payment = round(payment, 2)
+            self.state_distribution_data_dict[str(self.start_year) + "-" + str(self.end_year)] = []
+
+            for state in self.us_state_abbreviation:
+                state_name = self.us_state_abbreviation[state]
+                yearly_state_payment = total_payments_by_state[state_name]
 
                 new_data_entry = {
-                    "years": str(self.start_year) + "-" + str(self.end_year),
+                    "state": state,
                     "programs": [
                         {
                             "programName": "Agriculture Risk Coverage (ARC)",
@@ -304,35 +365,35 @@ class CommoditiesDataParser:
                     ],
                     "totalPaymentInPercentageNationwide": round(
                         (yearly_state_payment / total_payments_at_national_level) * 100, 2),
-                    "totalPaymentInDollars": yearly_state_payment
+                    "totalPaymentInDollars": round(yearly_state_payment, 2)
                 }
 
-                for data_tuple, program_payment in total_payments_by_program_by_state.items():
-                    data_tuple_state_name, program_description = data_tuple
+                program_payments_series = total_payments_by_program_by_state[state_name]
 
-                    if data_tuple_state_name == state_name:
-                        program_payment = round(program_payment, 2)
-                        program_percentage_nationwide = round(
-                            (program_payment / total_payments_by_program_at_national_level["payments"][
-                                program_description]) * 100, 2)
-                        program_percentage_within_state = round(
-                            (program_payment / total_payments_by_state[data_tuple_state_name]) * 100, 2)
-                        program_subprogram_name = self.find_program_by_subprogram(program_description)
+                for program_description, program_payment in program_payments_series.items():
+                    program_subprogram_name = self.find_program_by_subprogram(program_description)
+                    rounded_program_payment = round(program_payment, 2)
+                    program_percentage_nationwide = round(
+                        (rounded_program_payment / total_payments_by_program_at_national_level["payments"][
+                            program_description]) * 100, 2)
+                    program_percentage_within_state = round(
+                        (rounded_program_payment / total_payments_by_state[state_name]) * 100, 2)
 
-                        for program in new_data_entry["programs"]:
-                            if program["programName"] == program_subprogram_name:
-                                if len(self.programs_subprograms_mapping[program_subprogram_name]) == 0:
-                                    pass
-                                else:
-                                    program["subPrograms"].append({
-                                        "subProgramName": program_description,
-                                        "paymentInDollars": program_payment,
-                                        "paymentInPercentageNationwide": program_percentage_nationwide,
-                                        "paymentInPercentageWithinState": program_percentage_within_state
-                                    })
-                                program["programPaymentInDollars"] += program_payment
+                    for program in new_data_entry["programs"]:
+                        if program["programName"] == program_subprogram_name:
+                            if len(self.programs_subprograms_mapping[program_subprogram_name]) == 0:
+                                pass
+                            else:
+                                program["subPrograms"].append({
+                                    "subProgramName": program_description,
+                                    "paymentInDollars": rounded_program_payment,
+                                    "paymentInPercentageNationwide": program_percentage_nationwide,
+                                    "paymentInPercentageWithinState": program_percentage_within_state
+                                })
+                            program["programPaymentInDollars"] += rounded_program_payment
 
-                self.state_distribution_data_dict[state_name] = [new_data_entry]
+                self.state_distribution_data_dict[str(self.start_year) + "-" + str(self.end_year)].append(
+                    new_data_entry)
 
             # Add zero entries
             for state_name in self.state_distribution_data_dict:
@@ -352,10 +413,11 @@ class CommoditiesDataParser:
                         program["subPrograms"].sort(reverse=True,
                                                     key=lambda x: x["paymentInPercentageWithinState"])
 
-            # Sort states by decreasing order of percentages
-            self.state_distribution_data_dict = dict(sorted(self.state_distribution_data_dict.items(),
-                                                            key=lambda x: x[1][0]["totalPaymentInPercentageNationwide"],
-                                                            reverse=True))
+            # Sort states by decreasing order of totalPaymentInPercentageNationwide
+            for year in self.state_distribution_data_dict:
+                self.state_distribution_data_dict[year] = sorted(self.state_distribution_data_dict[year],
+                                                                 key=lambda x: x["totalPaymentInPercentageNationwide"],
+                                                                 reverse=True)
 
             # Write processed_data_dict as JSON data
             with open("commodities_state_distribution_data.json", "w") as output_json_file:
