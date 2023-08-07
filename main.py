f6ea817d4f5733f33bb9182d476e3e97b122c16e
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2023-08-07 12:44:42 -0500

    Add new Title 1 raw files and update source code to process it.

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from data_parser import DataParser
 
 if __name__ == '__main__':
-    commodities_data_parser = DataParser(2018, 2022, "Title 1: Commodities",
+    commodities_data_parser = DataParser(2014, 2021, "Title 1: Commodities",
                                          "title-1-commodities", "title_1_version_1.csv",
-                                         base_acres_csv_filename="baseacres_commodity_county_program.csv",
-                                         farm_payee_count_csv_filename="commodity_payments_counts.csv")
+                                         base_acres_csv_filename_arc_co="ARC-CO Base Acres by Program.csv",
+                                         base_acres_csv_filename_plc="PLC Base Acres by Program.csv",
+                                         farm_payee_count_csv_filename_arc_co="ARC-CO Recipients by Program.csv",
+                                         farm_payee_count_csv_filename_arc_ic="ARC-IC Recipients by Program.csv",
+                                         farm_payee_count_csv_filename_plc="PLC Recipients by Program.csv",
+                                         total_payment_csv_filename_arc_co="ARC-CO.csv",
+                                         total_payment_csv_filename_arc_ic="ARC-IC.csv",
+                                         total_payment_csv_filename_plc="PLC.csv"
+                                         )
+    commodities_data_parser.format_title_commodities_data()
     commodities_data_parser.parse_and_process()
