593b7d592cf35bfa8934c21b608114638903c36b
YONG WOOK KIM <ywkim312@users.noreply.github.com>
2024-02-06 16:40:32 -0600

    rename crp data

--- main.py
+++ main.py
@@ -20,26 +20,26 @@ if __name__ == '__main__':
     commodities_data_parser.format_title_commodities_data()
     commodities_data_parser.parse_and_process()
 
-    # crp_data_parser = DataParser(2018, 2022, "Title 2: Conservation: CRP",
-    #                              os.path.join("title-2-conservation", "crp"),
-    #                              "CRP_total_compiled_August_24_2023.csv")
-    # crp_data_parser.parse_and_process_crp()
-    #
-    # crop_insurance_data_parser = DataParser(2018, 2022, "Crop Insurance",
-    #                                         "crop-insurance", "ci_state_year_benefits 8-28-23.csv")
-    # crop_insurance_data_parser.parse_and_process_crop_insurance()
-    #
-    # acep_data_parser = AcepParser(2018, 2022, "Title 2: Conservation: ACEP",
-    #                               os.path.join("title-2-conservation", "acep"),
-    #                               "ACEP.csv")
-    #
-    # acep_data_parser.parse_and_process()
-    #
-    # rcpp_data_parser = RcppParser(2018, 2022, "Title 2: Conservation: ACEP",
-    #                               os.path.join("title-2-conservation", "rcpp"),
-    #                               "RCPP.csv")
-    #
-    # rcpp_data_parser.parse_and_process()
+    crp_data_parser = DataParser(2018, 2022, "Title 2: Conservation: CRP",
+                                 os.path.join("title-2-conservation", "crp"),
+                                 "CRP_total_compiled_August_24_2023.csv")
+    crp_data_parser.parse_and_process_crp()
+
+    crop_insurance_data_parser = DataParser(2018, 2022, "Crop Insurance",
+                                            "crop-insurance", "ci_state_year_benefits 8-28-23.csv")
+    crop_insurance_data_parser.parse_and_process_crop_insurance()
+
+    acep_data_parser = AcepParser(2018, 2022, "Title 2: Conservation: ACEP",
+                                  os.path.join("title-2-conservation", "acep"),
+                                  "ACEP.csv")
+
+    acep_data_parser.parse_and_process()
+
+    rcpp_data_parser = RcppParser(2018, 2022, "Title 2: Conservation: ACEP",
+                                  os.path.join("title-2-conservation", "rcpp"),
+                                  "RCPP.csv")
+
+    rcpp_data_parser.parse_and_process()
 
     dairy_disaster_parser = DairyDisasterParser(2014, 2021, "Title 1: Commodities: Dairy and Disaster",
                                                 "title-1-commodities", "Dairy-Disaster.csv")
