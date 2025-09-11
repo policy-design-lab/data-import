c8a707224ca380bbc778b82574ea14a94efb3f46
Ya-Lan Yang <ylyang@illinois.edu>
2025-09-11 16:19:46 -0500

    fix data path

--- main.py
+++ main.py
@@ -68,6 +68,6 @@ if __name__ == '__main__':
                                             "title-1-commodities/arcplc_model",
                                             "title-1-commodities/arcplc_model/CSVResultsCurrentFB06-27-2025.csv",
                                             "title-1-commodities/arcplc_model/CSVResultsProposedFB06-27-2025.csv",
-                                  "../title-1-commodities/arcplc_model/CSVResultsOBBBAFB08-04-2025.csv",
-                                  "../title-1-commodities/arcplc_model/CSVResultsProposedFB08-04-2025.csv")
+                                  "title-1-commodities/arcplc_model/CSVResultsOBBBAFB08-04-2025.csv",
+                                  "title-1-commodities/arcplc_model/CSVResultsProposedFB08-04-2025.csv")
     arc_plc_parser.parse_and_process()
